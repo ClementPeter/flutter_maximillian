16814b48ab015cfaa48e60728f49a521ea57c61c
carlos <clementpeter2000@gmail.com git config --global user.name ClementPeter>
2023-06-25 20:46:26 +0100

    “quizz_app”

--- quizz_app/lib/question_screen.dart
+++ quizz_app/lib/question_screen.dart
@@ -10,6 +10,14 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text('Your Question'),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 1'),
+        ),
+      ],
+    );
   }
 }
