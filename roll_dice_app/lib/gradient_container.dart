5cda74069bcd4e58c9257c73c3d7e3bc325ed6aa
ClementPeter <clementpeter2000@gmail.com>
2023-07-10 22:17:52 +0100

    🚀Roll_dice_App

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -1,24 +1,24 @@
-// import 'package:flutter/material.dart';
-// import 'package:roll_dice_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/dice_roller.dart';
 
-// //custom gradient background
-// class GradientContainer extends StatelessWidget {
-//   final List<Color> colors;
-//   const GradientContainer({super.key, required this.colors});
+//custom gradient background
+class GradientContainer extends StatelessWidget {
+  final List<Color> colors;
+  const GradientContainer({super.key, required this.colors});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         gradient: LinearGradient(
-//           colors: colors,
-//           begin: Alignment.topLeft,
-//           end: Alignment.bottomRight,
-//         ),
-//       ),
-//       child: const Center(
-//         child: DiceRoller(),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,45 +1,45 @@
-// import 'package:flutter/material.dart';
-// import 'package:roll_dice_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Roll Dice App',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Roll Dice App'),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Roll Dice App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Roll Dice App'),
+    );
+  }
+}
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-//   final String title;
+  final String title;
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: GradientContainer(
-//         colors: [
-//           Colors.deepPurple.shade200,
-//           Colors.deepPurple.shade400,
-//         ],
-//       ),
-//     );
-//   }
-// }
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GradientContainer(
+        colors: [
+          Colors.deepPurple.shade200,
+          Colors.deepPurple.shade400,
+        ],
+      ),
+    );
+  }
+}
