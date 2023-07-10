a12ea7fcaa6275d807fc2c0d1a6f8da3ac27e3f4
ClementPeter <clementpeter2000@gmail.com>
2023-07-10 22:09:52 +0100

    🚀Roll_dice_App

--- roll_dice_app/lib/dice_roller.dart
+++ roll_dice_app/lib/dice_roller.dart
@@ -1,43 +1,43 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
+// import 'package:flutter/material.dart';
+// import 'dart:math';
 
-//Declare global variable for random object
-final randomizer = Random();
+// //Declare global variable for random object
+// final randomizer = Random();
 
-class DiceRoller extends StatefulWidget {
-  const DiceRoller({super.key});
+// class DiceRoller extends StatefulWidget {
+//   const DiceRoller({super.key});
 
-  @override
-  State<DiceRoller> createState() => _DiceRollerState();
-}
+//   @override
+//   State<DiceRoller> createState() => _DiceRollerState();
+// }
 
-class _DiceRollerState extends State<DiceRoller> {
-  var currentDiceRoll = 1;
+// class _DiceRollerState extends State<DiceRoller> {
+//   var currentDiceRoll = 1;
 
-  //randomize button click
-  void rollDice() {
-    //generate a random number to shuffle the images in sync
-    setState(() {
-      currentDiceRoll = randomizer.nextInt(6) + 1;
-    });
-  }
+//   //randomize button click
+//   void rollDice() {
+//     //generate a random number to shuffle the images in sync
+//     setState(() {
+//       currentDiceRoll = randomizer.nextInt(6) + 1;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Image.asset("assets/images/dice-$currentDiceRoll.png"),
-        const SizedBox(height: 20),
-        TextButton(
-          onPressed: rollDice,
-          child: const Text(
-            "Roll Dice",
-            style: TextStyle(fontSize: 22, color: Colors.white),
-          ),
-        )
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       mainAxisSize: MainAxisSize.max,
+//       children: [
+//         Image.asset("assets/images/dice-$currentDiceRoll.png"),
+//         const SizedBox(height: 20),
+//         TextButton(
+//           onPressed: rollDice,
+//           child: const Text(
+//             "Roll Dice",
+//             style: TextStyle(fontSize: 22, color: Colors.white),
+//           ),
+//         )
+//       ],
+//     );
+//   }
+// }
