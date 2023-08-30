3b134846f514059a14b950a56850192714d35139
ClementPeter <clementpeter2000@gmail.com>
2023-08-30 17:40:38 +0100

    💰expense_tracker

--- expense_tracker/lib/new_expense.dart
+++ expense_tracker/lib/new_expense.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
+//UI for the showModalBottomSheet
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
@@ -13,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   TextEditingController titleController = TextEditingController();
   TextEditingController amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedValue = Category.food;
 
   // //show date picker
   void _presentDatePicker() async {
@@ -31,12 +33,11 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-  // //using .then
+  // //Date picker using .then
   // DateTime? _selectedDate;
   // void _presentDatePicker() {
   //   final now = DateTime.now();
   //   final firstDate = DateTime(now.year - 1, now.month, now.day);
-
   //   showDatePicker(
   //     context: context,
   //     initialDate: now,
@@ -85,9 +86,11 @@ class _NewExpenseState extends State<NewExpense> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(_selectedDate == null
-                      ? 'No Date Selected'
-                      : formatter.format(_selectedDate!)),
+                  Text(
+                    _selectedDate == null
+                        ? 'No Date Selected'
+                        : formatter.format(_selectedDate!),
+                  ),
                   IconButton(
                     onPressed: () {
                       print("_selectedDateOnpressed::::$_selectedDate::::");
@@ -99,6 +102,8 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+
+          const SizedBox(height: 20),
           // Row(
           //   children: [
           //     //amount textfield
@@ -117,33 +122,54 @@ class _NewExpenseState extends State<NewExpense> {
           //   ],
           // ),
           //Dropdown
-          Expanded(
-            child: Row(
-              children: [
-                const Text('Drop down'),
-                const Spacer(),
-                //save and cancel button
-                Row(
-                  children: [
-                    TextButton(
-                      child: const Text('cancel'),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        print('::::${titleController.text}::::');
-                        print('::::${amountController.text}::::');
-                      },
-                      child: const Text('Save Expense'),
-                    ),
-                    //Text('cancel'),
-                  ],
-                )
-              ],
-            ),
+          Row(
+            children: [
+              //const Text('Drop down'),
+              DropdownButton(
+                value: _selectedValue,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  print('drpDwn::: $value:::');
+                  if (value == null) {
+                    return;
+                  }
+
+                  setState(() {
+                    _selectedValue = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              //save and cancel button
+              Row(
+                children: [
+                  TextButton(
+                    child: const Text('cancel'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('::::${titleController.text}::::');
+                      print('::::${amountController.text}::::');
+                    },
+                    child: const Text('Save Expense'),
+                  ),
+                  //Text('cancel'),
+                ],
+              )
+            ],
           )
         ],
       ),
