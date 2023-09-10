b7ef5e10f22a258263fae40c2af5ee9afe4d8c5c
ClementPeter <clementpeter2000@gmail.com>
2023-09-10 19:02:28 +0100

    💰expense_tracker

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,140 +1,140 @@
-// import 'package:expense_tracker/expense_list.dart';
-// import 'package:expense_tracker/model/expense.dart';
-// import 'package:expense_tracker/new_expense.dart';
-// import 'package:flutter/material.dart';
+import 'package:expense_tracker/expense_list.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/new_expense.dart';
+import 'package:flutter/material.dart';
 
-// class Expenses extends StatefulWidget {
-//   const Expenses({super.key});
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
 
-//   @override
-//   State<Expenses> createState() => _ExpensesState();
-// }
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
 
-// class _ExpensesState extends State<Expenses> {
-//   //List of dummy expenses
-//   final List<Expense> _registeredExpense = [
-//     Expense(
-//       title: 'Travel Trip',
-//       amount: 1000,
-//       date: DateTime.now(),
-//       category: Category.travel,
-//     ),
-//     Expense(
-//       title: 'Flutter Course',
-//       amount: 20.0,
-//       date: DateTime.now(),
-//       category: Category.work,
-//     ),
-//     Expense(
-//       title: 'Cinema ',
-//       amount: 10.0,
-//       date: DateTime.now(),
-//       category: Category.leisure,
-//     )
-//   ];
+class _ExpensesState extends State<Expenses> {
+  //List of dummy expenses
+  final List<Expense> _registeredExpense = [
+    Expense(
+      title: 'Travel Trip',
+      amount: 1000,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Flutter Course',
+      amount: 20.0,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema ',
+      amount: 10.0,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
 
-//   //add New Expense function
-//   void _addExpense(Expense expense) {
-//     print("new Expense:::$expense::::");
-//     setState(() {
-//       _registeredExpense.add(expense);
-//     });
-//   }
+  //add New Expense function
+  void _addExpense(Expense expense) {
+    print("new Expense:::$expense::::");
+    setState(() {
+      _registeredExpense.add(expense);
+    });
+  }
 
-//   //remove expense function
-//   void _removeExpense(Expense expense) {
-//     //get index for undo feature
-//     final expenseIndex = _registeredExpense.indexOf(expense);
+  //remove expense function - snackbar and undo feature
+  void _removeExpense(Expense expense) {
+    //get index for undo feature
+    final expenseIndex = _registeredExpense.indexOf(expense);
 
-//     //remove item
-//     setState(() {
-//       _registeredExpense.remove(expense);
-//       return;
-//     });
+    //remove item
+    setState(() {
+      _registeredExpense.remove(expense);
+      return;
+    });
 
-//     //clearSnackBars stays before the snackbar usage to work
-//     ScaffoldMessenger.of(context).clearSnackBars();
+    //clearSnackBars stays before the snackbar usage to work
+    ScaffoldMessenger.of(context).clearSnackBars();
 
-//     ScaffoldMessenger.of(context).showSnackBar(
-//       SnackBar(
-//         duration: const Duration(seconds: 3),
-//         content: const Text('Expense deleted'),
-//         action: SnackBarAction(
-//           label: 'undo',
-//           onPressed: () {
-//             setState(() {
-//               print('::undo::');
-//               //insert removed item back using it's index
-//               _registeredExpense.insert(expenseIndex, expense);
-//             });
-//           },
-//         ),
-//       ),
-//     );
-//   }
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'undo',
+          onPressed: () {
+            setState(() {
+              print('::undo::');
+              //insert removed item back using it's index
+              _registeredExpense.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
 
-//   //show ModalBottomSheet
-//   void _openAddExpenseOverlay() {
-//     //print('modal pop up');
-//     showModalBottomSheet(
-//       context: context,
-//       //isScrollControlled: true, //Full screen ModalBottomSheet
-//       showDragHandle: true,
-//       useSafeArea: true,
-//       builder: (context) {
-//         return NewExpense(onAddExpense: _addExpense);
-//       },
-//     );
-//   }
+  //show ModalBottomSheet
+  void _openAddExpenseOverlay() {
+    //print('modal pop up');
+    showModalBottomSheet(
+      context: context,
+      //isScrollControlled: true, //Full screen ModalBottomSheet
+      showDragHandle: true,
+      useSafeArea: true,
+      builder: (context) {
+        return NewExpense(onAddExpense: _addExpense);
+      },
+    );
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     Widget mainContent = const Center(
-//       child: Text('No expense found, Start adding some'),
-//     );
-//     if (_registeredExpense.isNotEmpty) {
-//       mainContent = ExpenseList(
-//         expense: _registeredExpense,
-//         onRemoveExpense: _removeExpense,
-//       );
-//     }
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expense found, Start adding some'),
+    );
+    if (_registeredExpense.isNotEmpty) {
+      mainContent = ExpenseList(
+        expense: _registeredExpense,
+        onRemoveExpense: _removeExpense,
+      );
+    }
 
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           'Expense Tracker',
-//           style: Theme.of(context).textTheme.titleLarge,
-//         ),
-//         actions: [
-//           IconButton(
-//             onPressed: _openAddExpenseOverlay,
-//             icon: const Icon(Icons.add),
-//           )
-//         ],
-//       ),
-//       body: Column(
-//         children: [
-//           const Text('Chart'),
-//           Expanded(child: mainContent),
-//           //if you use my expense List implementation
-//           // Expanded(
-//           //   child: ExpenseList(
-//           //     expense: _registeredExpense,
-//           //     onRemoveExpense: _removeExpense,
-//           //   ),
-//           // ),
-//         ],
-//       ),
-//       // body: Column(
-//       //   children: [
-//       //     Text('Chart'),
-//       //     Text('Expenses'),
-//       //   ],
-//       // ),
-//       // body: ListView(
-//       //   children:
-//       //       _registeredExpense.map((expense) => Text(expense.title)).toList(),
-//       // ),
-//     );
-//   }
-// }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Expense Tracker',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(child: mainContent),
+          //if you use my expense List implementation
+          // Expanded(
+          //   child: ExpenseList(
+          //     expense: _registeredExpense,
+          //     onRemoveExpense: _removeExpense,
+          //   ),
+          // ),
+        ],
+      ),
+      // body: Column(
+      //   children: [
+      //     Text('Chart'),
+      //     Text('Expenses'),
+      //   ],
+      // ),
+      // body: ListView(
+      //   children:
+      //       _registeredExpense.map((expense) => Text(expense.title)).toList(),
+      // ),
+    );
+  }
+}
