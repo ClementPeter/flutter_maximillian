696acc247575e92b0c37fe6022da8f5809d395cf
ClementPeter <clementpeter2000@gmail.com>
2024-01-16 22:06:25 +0100

    💰expense_tracker

--- expense_tracker/lib/expense_list.dart
+++ expense_tracker/lib/expense_list.dart
@@ -1,120 +1,120 @@
-// import 'package:expense_tracker/main.dart';
-// import 'package:expense_tracker/model/expense.dart';
-// import 'package:expense_tracker/widgets/expense_list/expense_item.dart';
-// import 'package:flutter/material.dart';
+import 'package:expense_tracker/main.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/expense_list/expense_item.dart';
+import 'package:flutter/material.dart';
 
-// // //UI to hold scroll List of Items
+// //UI to hold scroll List of Items
+class ExpenseList extends StatelessWidget {
+  final List<Expense> expense;
+  final void Function(Expense expense) onRemoveExpense;
+
+  const ExpenseList(
+      {super.key, required this.expense, required this.onRemoveExpense});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expense.length,
+      itemBuilder: (context, index) {
+        //print("list view builder : $index");
+        // return ListTile(
+        //   title: Text(expense[index].title),
+        //   subtitle: Text(expense[index].amount.toString()),
+        // );
+        return Dismissible(
+          //key: Key(expense[index].id),
+          key: ValueKey(expense[index]),
+          //background: Container(color: kColorScheme.error.withOpacity(0.75)),
+          background: Container(
+            height: 10,
+            //margin: const EdgeInsets.symmetric(horizontal: 10),
+            margin: Theme.of(context).cardTheme.margin,
+            // margin: EdgeInsets.symmetric(
+            //     horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+            padding: const EdgeInsets.only(right: 20),
+            decoration: BoxDecoration(
+              color: kColorScheme.error.withOpacity(0.75),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              children: [
+                const Spacer(),
+                Icon(
+                  Icons.delete_outline,
+                  color: kColorScheme.onError,
+                  //size: 36.0,
+                ),
+              ],
+            ),
+          ),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            onRemoveExpense(expense[index]);
+          },
+          //dragStartBehavior: DragStartBehavior.start,
+          child: ExpenseItem(
+            expense: expense[index],
+          ),
+        );
+      },
+    );
+  }
+}
+
+///My implementation
+
 // class ExpenseList extends StatelessWidget {
-//   final List<Expense> expense;
-//   final void Function(Expense expense) onRemoveExpense;
-
 //   const ExpenseList(
 //       {super.key, required this.expense, required this.onRemoveExpense});
 
+//   final List<Expense> expense;
+//   final void Function(Expense expense) onRemoveExpense;
+
 //   @override
 //   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: expense.length,
-//       itemBuilder: (context, index) {
-//         //print("list view builder : $index");
-//         // return ListTile(
-//         //   title: Text(expense[index].title),
-//         //   subtitle: Text(expense[index].amount.toString()),
-//         // );
-//         return Dismissible(
-//           //key: Key(expense[index].id),
-//           key: ValueKey(expense[index]),
-//           //background: Container(color: kColorScheme.error.withOpacity(0.75)),
-//           background: Container(
-//             height: 10,
-//             //margin: const EdgeInsets.symmetric(horizontal: 10),
-//             margin: Theme.of(context).cardTheme.margin,
-//             // margin: EdgeInsets.symmetric(
-//             //     horizontal: Theme.of(context).cardTheme.margin!.horizontal),
-//             padding: const EdgeInsets.only(right: 20),
-//             decoration: BoxDecoration(
-//               color: kColorScheme.error.withOpacity(0.75),
-//               borderRadius: BorderRadius.circular(10),
-//             ),
-//             child: Row(
-//               children: [
-//                 const Spacer(),
-//                 Icon(
-//                   Icons.delete_outline,
-//                   color: kColorScheme.onError,
-//                   //size: 36.0,
+//     return expense.isEmpty
+//         ? const Center(
+//             child: Text('No expense found, Start adding some'),
+//           )
+//         : ListView.builder(
+//             itemCount: expense.length,
+//             itemBuilder: (context, index) {
+//               //print("list view builder : $index");
+//               // return ListTile(
+//               //   title: Text(expense[index].title),
+//               //   subtitle: Text(expense[index].amount.toString()),
+//               // );
+//               return Dismissible(
+//                 //key: Key(expense[index].id),
+//                 key: ValueKey(expense[index]),
+      //  background: Container(
+      //       margin: const EdgeInsets.symmetric(horizontal: 10),
+      //       padding: const EdgeInsets.symmetric(horizontal: 20),
+      //       decoration: BoxDecoration(
+      //         color: kColorScheme.error.withOpacity(0.75),
+      //         borderRadius: BorderRadius.circular(10),
+      //       ),
+      //       child: Row(
+      //         children: [
+      //           const Spacer(),
+      //           Icon(
+      //             Icons.delete_outline,
+      //             color: kColorScheme.onError,
+      //             //size: 36.0,
+      //           ),
+      //         ],
+      //       ),
+          // ),
+//                 direction: DismissDirection.endToStart,
+//                 onDismissed: (direction) {
+//                   onRemoveExpense(expense[index]);
+//                 },
+//                 dragStartBehavior: DragStartBehavior.down,
+//                 child: ExpenseItem(
+//                   expense: expense[index],
 //                 ),
-//               ],
-//             ),
-//           ),
-//           direction: DismissDirection.endToStart,
-//           onDismissed: (direction) {
-//             onRemoveExpense(expense[index]);
-//           },
-//           //dragStartBehavior: DragStartBehavior.start,
-//           child: ExpenseItem(
-//             expense: expense[index],
-//           ),
-//         );
-//       },
-//     );
+//               );
+//             },
+//           );
 //   }
 // }
-
-// ///My implementation
-
-// // class ExpenseList extends StatelessWidget {
-// //   const ExpenseList(
-// //       {super.key, required this.expense, required this.onRemoveExpense});
-
-// //   final List<Expense> expense;
-// //   final void Function(Expense expense) onRemoveExpense;
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return expense.isEmpty
-// //         ? const Center(
-// //             child: Text('No expense found, Start adding some'),
-// //           )
-// //         : ListView.builder(
-// //             itemCount: expense.length,
-// //             itemBuilder: (context, index) {
-// //               //print("list view builder : $index");
-// //               // return ListTile(
-// //               //   title: Text(expense[index].title),
-// //               //   subtitle: Text(expense[index].amount.toString()),
-// //               // );
-// //               return Dismissible(
-// //                 //key: Key(expense[index].id),
-// //                 key: ValueKey(expense[index]),
-//       //  background: Container(
-//       //       margin: const EdgeInsets.symmetric(horizontal: 10),
-//       //       padding: const EdgeInsets.symmetric(horizontal: 20),
-//       //       decoration: BoxDecoration(
-//       //         color: kColorScheme.error.withOpacity(0.75),
-//       //         borderRadius: BorderRadius.circular(10),
-//       //       ),
-//       //       child: Row(
-//       //         children: [
-//       //           const Spacer(),
-//       //           Icon(
-//       //             Icons.delete_outline,
-//       //             color: kColorScheme.onError,
-//       //             //size: 36.0,
-//       //           ),
-//       //         ],
-//       //       ),
-//           // ),
-// //                 direction: DismissDirection.endToStart,
-// //                 onDismissed: (direction) {
-// //                   onRemoveExpense(expense[index]);
-// //                 },
-// //                 dragStartBehavior: DragStartBehavior.down,
-// //                 child: ExpenseItem(
-// //                   expense: expense[index],
-// //                 ),
-// //               );
-// //             },
-// //           );
-// //   }
-// // }
--- expense_tracker/lib/widgets/expense_list/expense_item.dart
+++ expense_tracker/lib/widgets/expense_list/expense_item.dart
@@ -1,44 +1,44 @@
-// import 'package:expense_tracker/model/expense.dart';
-// import 'package:flutter/material.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
 
-// //Expense item - card like UI containing expense item
-// class ExpenseItem extends StatelessWidget {
-//   const ExpenseItem({super.key, required this.expense});
+//Expense item - card like UI containing expense item
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
 
-//   final Expense expense;
+  final Expense expense;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       margin: Theme.of(context).cardTheme.margin,
-//       child: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           crossAxisAlignment: CrossAxisAlignment.end,
-//           children: [
-//             Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Text(
-//                   expense.title,
-//                   //style: const TextStyle(fontWeight: FontWeight.bold),
-//                   style: Theme.of(context).textTheme.titleLarge,
-//                 ),
-//                 const SizedBox(height: 5),
-//                 Text(expense.amount.toStringAsFixed(2))
-//               ],
-//             ),
-//             Row(
-//               children: [
-//                 Icon(categoryIcon[expense.category]),
-//                 const SizedBox(width: 5),
-//                 Text(expense.formattedDate)
-//               ],
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: Theme.of(context).cardTheme.margin,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  expense.title,
+                  //style: const TextStyle(fontWeight: FontWeight.bold),
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 5),
+                Text(expense.amount.toStringAsFixed(2))
+              ],
+            ),
+            Row(
+              children: [
+                Icon(categoryIcon[expense.category]),
+                const SizedBox(width: 5),
+                Text(expense.formattedDate)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
